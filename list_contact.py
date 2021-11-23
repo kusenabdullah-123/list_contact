b8cb357bece5e13c510a411fe838ec56eb59de21
Kusen Abdullah <kusena322@gmail.com>
2021-11-23 12:16:10 +0700

    add and import module

---
+++ list_contact.py
@@ -0,0 +1,6 @@
+from module_contact import show_contact
+from module_contact import add_contact
+from module_contact import delete_contact
+from module_contact import find_contact
+
+list_menu = ['List Contact']
---
+++ module_contact.py
@@ -0,0 +1,14 @@
+def show_contact():
+    print('hello')
+
+
+def add_contact():
+    print('hello')
+
+
+def delete_contact():
+    print('hello')
+
+
+def find_contact():
+    print('find')
