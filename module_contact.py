458edaa2c04f2fc96cfaa58b6677ed0d1e2ac51a
Kusen Abdullah <kusena322@gmail.com>
2021-11-23 13:15:05 +0700

    module show contact and add

--- module_contact.py
+++ module_contact.py
@@ -1,9 +1,26 @@
-def show_contact():
-    print('hello')
+def show_contact(contacts):
+    if(len(contacts) <= 0):
+        print('========Contact========')
+        print('404 Contact Not Found')
+        print('====================')
+    else:
+        print('========Contact========')
+        for contact in contacts:
+            print('--------------------')
+            for key, value in contact.items():
+                print(f'{key}: {value} ')
+        print('====================')
 
 
 def add_contact():
-    print('hello')
+    nama = input('Namanya Siapa?')
+    no = input('No nya dong?')
+    email = input('sekalian email nya ya')
+    return {
+        'nama': nama,
+        'no': no,
+        'email': email
+    }
 
 
 def delete_contact():
